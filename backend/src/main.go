b76328d34552bea50d8c0fb08c2d71ddad5dad70
pisanvs <maxmorel@pisanvs.cl>
2021-11-01 14:53:38 -0300

    registration finally works

--- backend/src/main.go
+++ backend/src/main.go
@@ -15,6 +15,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/go-playground/validator/v10"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // CONSTANTS
@@ -34,6 +36,15 @@ type RegisterForm struct {
 	email    string `validate:"required,email"`
 }
 
+type DBUser struct {
+	username     string `validate:"required,printascii "`
+	password     string `validate:"required,printascii"`
+	name         string `validate:"required,alpha"`
+	email        string `validate:"required,email"`
+	creationDate time.Time
+	lastLogin    time.Time
+}
+
 // VARIABLES
 
 var client *mongo.Client
@@ -44,11 +55,20 @@ var usersDB *mongo.Collection
 
 var validate *validator.Validate
 
+// hash password using bcrypt
+func hashPassword(password string) (string, error) {
+	bcryptPassword, err := bcrypt.GenerateFromPassword([]byte(password), 13)
+	if err != nil {
+		return "", err
+	}
+	return string(bcryptPassword), nil
+}
+
 func main() {
 
 	validate = validator.New()
 
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://172.16.150.17:27017"))
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://172.16.150.17:27017/?readPreference=primary&connect=direct&sslInsecure=true"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -64,10 +84,9 @@ func main() {
 	fmt.Println(client.ListDatabaseNames(ctx, bson.M{}))
 
 	mainMux := mux.NewRouter().StrictSlash(true)
-	mainMux.PathPrefix(apiRoot)
-	mainMux.HandleFunc("/auth/session", session)
-	mainMux.HandleFunc("/auth/login", login)
-	mainMux.HandleFunc("/auth/register", register)
+	mainMux.HandleFunc(apiRoot+"/auth/session", session)
+	mainMux.HandleFunc(apiRoot+"/auth/login", login)
+	mainMux.HandleFunc(apiRoot+"/auth/register", register)
 
 	log.Fatal(http.ListenAndServe(":8000", mainMux))
 }
@@ -98,24 +117,62 @@ func login(w http.ResponseWriter, r *http.Request) {
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
+	// check if method is POST
 	if r.Method != "POST" {
 		w.WriteHeader(405)
 		w.Write([]byte("Wrong method, use POST for register endpoint"))
 		return
 	}
 
+	// decode request body
 	form := RegisterForm{
-		name:     r.Form.Get("name"),
-		username: r.Form.Get("username"),
-		password: r.Form.Get("password"),
-		email:    r.Form.Get("email"),
+		name:     r.FormValue("name"),
+		username: r.FormValue("username"),
+		password: r.FormValue("password"),
+		email:    r.FormValue("email"),
 	}
 
+	// Validate form
 	err := validate.Struct(form)
 	if err != nil {
 		w.WriteHeader(400)
 		w.Write([]byte("Validation of data Failed"))
 		return
 	}
+
+	// Check if username already exists
+	var result bson.M
+	err = usersDB.FindOne(context.TODO(), bson.D{{"username", form.username}}).Decode(&result)
+	if err != nil {
+		fmt.Print(err)
+	}
+	if result != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("Username already exists"))
+		return
+	}
+
+	// Hash password
+	hashedPass, err := hashPassword(form.password)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("Error hashing password. Please try again, if it still doesn't work, submit an issue on Github"))
+		return
+	}
+
+	// Create new user
+	newUser := DBUser{
+		username:     form.username,
+		password:     hashedPass,
+		name:         form.name,
+		email:        form.email,
+		creationDate: time.Now(),
+		lastLogin:    time.Now(),
+	}
+
+	usersDB.InsertOne(ctx, bson.D{{"username", newUser.username}, {"password", newUser.password}, {"name", newUser.name}, {"email", newUser.email}, {"creationDate", newUser.creationDate}, {"lastLogin", newUser.lastLogin}})
+
+	w.WriteHeader(200)
+	w.Write([]byte("User created"))
 
 }
--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -5,18 +5,43 @@ interface Props {
 }
 interface State {
     isValid: boolean;
+    passwordState: string;
 }
 
+const allowedPasswordsChars: string[] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '[', '}', ']', '|', ':', ';', '"', '\'', '<', ',', '>', '.', '?', '/', '~', '`'
+];
+
 export default class register extends Component<Props, State> {
+    passwordConfirmation: React.RefObject<HTMLInputElement>;
+    password: React.RefObject<HTMLInputElement>;
     constructor(props: Props) {
         super(props)
         this.state = {
-            isValid: false
+            isValid: false,
+            passwordState: ""
         }
+        this.password = React.createRef();
+        this.passwordConfirmation = React.createRef();
     }
 
-    onPasswordUpdate() {
+    onPasswordUpdate(e: React.ChangeEvent<HTMLInputElement>) {
+        // check if password is too short or too long
+        if (this.password.current?.value?.length !== undefined && this.password.current.value.length < 8) {
+            this.setState({ isValid: false, passwordState: "Password is too short" })
+        } else if (this.password.current?.value?.length !== undefined && this.password.current.value.length > 64) {
+            this.setState({ isValid: false, passwordState: "Password is too long" })
+        }
 
+        // check if password contains invalid characters
+        this.password.current?.value?.split("").forEach(char => {
+            if (!allowedPasswordsChars.includes(char)) {
+                this.setState({ isValid: false, passwordState: "Password contains invalid characters" })
+            }
+        })
+
+        // Check if both passwords match
+        this.password.current?.value === this.passwordConfirmation.current?.value ? this.setState({ isValid: true, passwordState: "" }) : this.setState({ isValid: false, passwordState: "Passwords do not match" })
     }
 
     render() {
@@ -26,7 +51,7 @@ export default class register extends Component<Props, State> {
 
                     <h2 style={{ justifySelf: 'center' }}>Register</h2>
 
-                    <form action="noteer.local/api/v1/auth/register" method="post">
+                    <form action="http://api.noteer.local/api/v1/auth/register" method="post">
                         <label htmlFor="name">Name:</label>
                         <br />
                         <input type="text" name="name" id="name" className="register-input" />
@@ -50,17 +75,30 @@ export default class register extends Component<Props, State> {
 
                         <label htmlFor="password">Password:</label>
                         <br />
-                        <input type="password" name="password" id="password" className="register-input" />
+                        <input type="password" name="password" id="password" ref={this.password} className="register-input" />
 
                         <br />
                         <br />
 
-                        <label htmlFor="password">Password:</label>
+                        <label htmlFor="passwordConfirmation">Confirm Password:</label>
                         <br />
-                        <input type="password" name="password" id="password" className="register-input" />
+                        <input type="password" name="passwordConfirmation" id="passwordConfirmation" ref={this.passwordConfirmation} className="register-input" onChange={(e) => this.onPasswordUpdate(e)} />
 
                         <br />
+
+                        {
+                            this.state.passwordState !== "" ? <p className="register-error">{this.state.passwordState}</p> : null
+                        }
+
+                        <br />
+                        <br />
+
+
                         <input type="submit" name="submit" id="submit" value="Submit" className="register-input" disabled={!this.state.isValid} />
+
+                        <br />
+                        <br />
+
                     </form>
                 </div>
             </div>
